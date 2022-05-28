184804879bdc44283b40f91def97636c20172aaf
tesfu <tesfu9503@gmail.com>
2022-05-28 15:48:17 +0300

    make it better

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 import express from 'express'
 import mongoose from'mongoose'
 import cors from 'cors'
-import cookieSession from 'cookie-session'
-import passport from'passport'
 import dotenv from 'dotenv'
-import pass from './controllers/pass.js'
 import user from './routes/user.js'
 import food from './routes/food.js'
 import resturant from './routes/resturant.js'
 import order from './routes/order.js'
+
+
 dotenv.config();
 const app = express()
-app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000',credentials:true,methods:"GET,POST,PUT,DELETE,HEAD",optionSuccessStatus:200}))
-app.use(cookieSession({name:'session',keys:['abdi'],maxAge:24*60*60*100}))
-app.use(passport.initialize())
-app.use(passport.session())
+
+
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('db connection successfllul'))
 .then((err)=>{console.log(err)})
-
+app.use(cors())
+app.use(express.json({ limit: '30mb', extended: true }))
 app.use('/api/order',order)
 app.use('/api/user',user)
 app.use('/api/food',food)
 app.use('/api/resturant',resturant)
-app.get('/api/',(req,res)=>{
-    res.send("hello stackoverflow")
+
+app.get('/',(req,res)=>{
+    res.send("successful")
 })
 
 const PORT=process.env.port || 5000 
